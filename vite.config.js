e9430669e7cefca853db365e0478625b2d47e6b7
elabra1 <137736115+elabra1@users.noreply.github.com>
2025-10-12 13:08:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/toDo/', // 👈 this must match your repo name
+  base: '/reactjs-todolist/', // 👈 this must match your repo name
 })
