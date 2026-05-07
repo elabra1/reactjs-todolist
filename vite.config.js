bfb0c8e1066c0692b7c242cc23060b795c8a30c9
Es Labra <eslabra@Ess-MacBook-Pro.attlocal.net>
2026-05-07 14:29:16 -0400

    fix merge conflict in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reactjs-todolist/', // 👈 this must match your repo name
+  base: '/', 
 })
